c68d6bc3890c5fa9754c1d2180c12710e17401b4
Monigaur8 <153185901+Monigaur8@users.noreply.github.com>
2024-09-13 17:18:40 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,10 @@
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
+    if (selectHeader) {
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
+  }}
 
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
