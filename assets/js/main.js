9f547f644e6d1d8ffc52ef4936196b8ff3c4bf00
Monigaur8 <153185901+Monigaur8@users.noreply.github.com>
2024-09-13 16:55:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,14 @@
    * Mobile nav toggle
    */
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
-
+if (mobileNavToggleBtn) {
   function mobileNavToogle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     mobileNavToggleBtn.classList.toggle('bi-list');
     mobileNavToggleBtn.classList.toggle('bi-x');
   }
   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-
+}
   /**
    * Hide mobile nav on same-page/hash links
    */
