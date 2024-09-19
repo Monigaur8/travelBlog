8a16aa718af4496daefdc6a10cb55894cae3640d
Monigaur8 <mgaur1586@gmail.com>
2024-09-19 15:12:54 +0530

    css update

--- travel_page_include/travelPages.js
+++ travel_page_include/travelPages.js
@@ -3,7 +3,7 @@
  const textData = {
     "text-box-1": "The Lal Bahadur Shastri National Academy of Administration (LBSNAA), Mussoorie is a premier training institution",
     "text-box-2": "South Asian Network Operators Group (SANOG) is a regional bi-annual event that brings together network operators,",
-    "text-box-3": " The Academy is located at Mussoorie, a hill station at a height of little above 6580 ft.",
+    "text-box-3": "The Academy is located at Mussoorie, a hill station at a height of little above 6580 ft.",
     "text-box-4": "Finally, this is the text for box 4, loaded at runtime."
 };
 
